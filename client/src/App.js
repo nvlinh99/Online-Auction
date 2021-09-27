997b2f083287cd9ee0107e82f2cc327e53eba6d3
Thanh Hai <hthai2201@gmail.com>
2021-09-28 00:00:20 +0700

    fix webpack

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, useMemo } from 'react'
 import { useRoutes } from 'react-router-dom'
 import { getRoutes } from 'routes'
+
 import 'styles/global.scss'
 const App = () => {
   const userRoutes = useMemo(() => getRoutes(), [])
   const routes = useRoutes(userRoutes)
+  console.log(process.env.API_URL)
 
   return <div>{routes}</div>
 }
