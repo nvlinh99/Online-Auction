f6d2d3912fb80df5871d695e00b2dd76d3f509a5
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2021-09-27 15:13:10 +0700

    test cicd

---
+++ api/src/test/routes.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+const superTest = require('supertest')
+
+const configuration = require('../configuration')
+
+const hostServer = configuration.server.host
+const server = superTest.agent(hostServer)
+
+describe('Sample test', function () {
+  it('should return home page - test content type', function (done) {
+    server.get('/')
+      .expect('Content-type', /text/)
+      .end(function (err, res) {
+        done()
+      })
+  })
+
+  it('should return home page - test status', function (done) {
+    server.get('/')
+      .expect(200)
+      .end(function (err, res) {
+        done()
+      })
+  })
+})
