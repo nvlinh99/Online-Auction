1454b3a3e3993af35534c5212f193406ff89624a
Thanh Hai <hthai2201@gmail.com>
2021-09-28 08:29:26 +0700

    reset password

--- api/src/route/user/index.js
+++ api/src/route/user/index.js
@@ -1,21 +1,23 @@
-const { Router, } = require('express')
-const loginController = require('./login')
-const registerController = require('./register')
-const registerConfirmationHandler = require('./register-confirmation')
-const forgetPasswordHandler = require('./forget-password')
-const updateHandler = require('./update-info')
-const authHandler = require('./auth')
+const { Router } = require("express");
+const loginController = require("./login");
+const registerController = require("./register");
+const registerConfirmationHandler = require("./register-confirmation");
+const forgetPasswordHandler = require("./forget-password");
+const resetPasswordHandler = require("./reset-password");
+const updateHandler = require("./update-info");
+const authHandler = require("./auth");
 
-exports.path = '/users'
+exports.path = "/users";
 
-const userRouter = Router()
-userRouter.post('/login', loginController)
-userRouter.post('/register', registerController)
-userRouter.get('/register/confirmation', registerConfirmationHandler)
-userRouter.post('/forget-password', forgetPasswordHandler)
+const userRouter = Router();
+userRouter.post("/login", loginController);
+userRouter.post("/register", registerController);
+userRouter.get("/register/confirmation", registerConfirmationHandler);
+userRouter.post("/forget-password", forgetPasswordHandler);
+userRouter.post("/reset-password", resetPasswordHandler);
 
 // Auth route: Action require logged in user
-userRouter.use(authHandler.authorize)
-userRouter.post('/update-info', updateHandler)
+userRouter.use(authHandler.authorize);
+userRouter.post("/update-info", updateHandler);
 
-exports.router = userRouter
+exports.router = userRouter;
