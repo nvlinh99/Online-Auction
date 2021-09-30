2d557d8d7ca40356fe9c239568e1196ec0b1d1bb
Thanh Hai <hthai2201@gmail.com>
2021-09-30 07:57:52 +0700

    remove react-notification

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,22 @@ import { Typography } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { categorySelector } from 'store/category'
 import { getCategoriesFromAPI } from 'store/category/action'
-
+import { makeStyles } from '@mui/styles'
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      background: theme.palette.success.main,
+    },
+  }
+})
 const HomePage = () => {
+  const classes = useStyles()
   const allCategories = useSelector(categorySelector.selectCategories)
   useEffect(() => {
     getCategoriesFromAPI()
   }, [])
   return (
-    <div>
+    <div className={classes.container}>
       <h1 className='bg-[#ff1111] text-lg'>
         <Typography variant='h1'>Hello home 123</Typography>
       </h1>
--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,93 +1,100 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react'
-import { Button, Typography, TextField, Box, FormControlLabel, Alert, Checkbox, } from '@mui/material'
+import {
+  Button,
+  Typography,
+  TextField,
+  Box,
+  FormControlLabel,
+  Alert,
+  Checkbox,
+} from '@mui/material'
 import { useLocation, useNavigate, Link } from 'react-router-dom'
 import { login } from 'services/userApi'
 import * as Validation from 'utils/validation'
-import { NotificationManager } from 'react-notifications';
-import qs from 'querystring';
+import qs from 'querystring'
+import { toast } from 'react-toastify'
 
 export default function LoginView() {
-	const [loading, setLoading] = useState(false)
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
   const [loginStatus, setLoginStatus] = useState({
     loading: false,
     error: null,
     success: false,
-  });
+  })
   const [formValues, setFormValues] = useState({
     email: '',
     password: '',
-    rememeber:false,
+    rememeber: false,
     submiting: false,
-  });
+  })
   const [formError, setFormErrors] = useState({
     email: '',
     password: '',
     submiting: '',
-  });
+  })
 
-
-  const handleChange = e => {
-    const { name, value,checked } = e.target;
-    if (name === "remember") {
-      setFormValues({ ...formValues, [name]: checked });
-      return 
+  const handleChange = (e) => {
+    const { name, value, checked } = e.target
+    if (name === 'remember') {
+      setFormValues({ ...formValues, [name]: checked })
+      return
     }
-    setFormValues({ ...formValues, [name]: value });
-  };
-  const handleSubmit = async e => {
-		setLoading(true)
-    e.preventDefault();
-    const { email, password, remember } = formValues;
-   
+    setFormValues({ ...formValues, [name]: value })
+  }
+  const handleSubmit = async (e) => {
+    setLoading(true)
+    e.preventDefault()
+    const { email, password, remember } = formValues
+
     setLoginStatus({
       error: null,
       success: false,
       loading: true,
-    });
+    })
     try {
-      const [succeeded, data] = await login({ email, password });
+      const [succeeded, data] = await login({ email, password })
       if (!succeeded) {
         setLoginStatus({
           error: 'Email hoặc mật khẩu không hợp lệ.',
           success: false,
           loading: false,
-        });
-        return;
+        })
+        return
       }
-      const result = data || {};
+      const result = data || {}
       setLoginStatus({
         error: null,
         success: true,
         loading: false,
-      });
+      })
       if (result.token) {
-        localStorage.setItem('USER_TOKEN', result.token);
-        localStorage.setItem('USER_INFO', JSON.stringify(result.user));
+        localStorage.setItem('USER_TOKEN', result.token)
+        localStorage.setItem('USER_INFO', JSON.stringify(result.user))
         if (remember) {
-          localStorage.setItem('EMAIL', email);
-          localStorage.setItem('PASSWORD', password);
+          localStorage.setItem('EMAIL', email)
+          localStorage.setItem('PASSWORD', password)
         }
 
-        const { retRef } = qs.parse(location.search.slice(1));
-        NotificationManager.success('Đăng nhập thành công', null, 1500);
-        navigate(retRef || '/app');
+        const { retRef } = qs.parse(location.search.slice(1))
+        toast.success('Đăng nhập thành công', null, 1500)
+        navigate(retRef || '/app')
       }
     } catch (error) {
       setLoginStatus({
         error: error.toString(),
         success: false,
         loading: false,
-      });
+      })
     }
-  };
+  }
   useEffect(() => {
-    const email = localStorage.getItem('EMAIL');
-    const password = localStorage.getItem('PASSWORD');
-    setFormValues({ email, password });
-  }, []);
+    const email = localStorage.getItem('EMAIL')
+    const password = localStorage.getItem('PASSWORD')
+    setFormValues({ email, password })
+  }, [])
 
-	return (
+  return (
     <div className='w-full h-screen flex justify-center items-center'>
       <Box
         sx={{
@@ -104,88 +111,86 @@ export default function LoginView() {
             Đăng nhập
           </Typography>
         </div>
-				{loginStatus.error && (
-					<Alert severity="error">
-						{loginStatus.error}
-					</Alert>
-				)}
-				<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						id="email"
-						label="Email"
-						name="email"
-						autoComplete="email"
-						autoFocus
-						value={formValues.email}
-						onChange={handleChange}
-						helperText={formError.email}
-						error={!!formError.email}
-					/>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						name="password"
-						label="Mật khẩu"
-						type="password"
-						id="password"
-						autoComplete="current-password"
-						value={formValues.password}
-						onChange={handleChange}
-						helperText={formError.password}
-						error={!!formError.password}
-					/>
-					<FormControlLabel
-						control={
-							<Checkbox
-								name="remember"
-								value = "true"
-								color="primary"
-								checked={formValues.remember}
-								onChange={handleChange}
-							/>
-						}
-						label="Ghi nhớ tôi"
-					/>
-					<Button
-						disabled={loading}
-						onClick={handleSubmit}
-						className='!mb-5'
-						fullWidth
-						variant='contained'
-					>
-						Đăng nhập
-					</Button>
-					<p style={{ textAlign: 'center' }}>
-						<span>Chưa có tài khoản? </span>
-						<Link
-							disabled={loading}
-							style={{
-								[loading ? 'pointerEvents' : '']: 'none',
-								textDecoration: 'underline',
-								textAlign: 'center',
-							}}
-							to='/register'
-						>
-							Đăng ký
-						</Link>
-					</p>
-					<p style={{ textAlign: 'center' }}>
-						<Link
-							disabled={loading}
-							style={{
-								[loading ? 'pointerEvents' : '']: 'none',
-								textDecoration: 'underline',
-								textAlign: 'center',
-							}}
-							to='/forget-password'
-						>
-							Quên mật khẩu
-						</Link>
+        {loginStatus.error && (
+          <Alert severity='error'>{loginStatus.error}</Alert>
+        )}
+        <TextField
+          variant='outlined'
+          margin='normal'
+          required
+          fullWidth
+          id='email'
+          label='Email'
+          name='email'
+          autoComplete='email'
+          autoFocus
+          value={formValues.email}
+          onChange={handleChange}
+          helperText={formError.email}
+          error={!!formError.email}
+        />
+        <TextField
+          variant='outlined'
+          margin='normal'
+          required
+          fullWidth
+          name='password'
+          label='Mật khẩu'
+          type='password'
+          id='password'
+          autoComplete='current-password'
+          value={formValues.password}
+          onChange={handleChange}
+          helperText={formError.password}
+          error={!!formError.password}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name='remember'
+              value='true'
+              color='primary'
+              checked={formValues.remember}
+              onChange={handleChange}
+            />
+          }
+          label='Ghi nhớ tôi'
+        />
+        <Button
+          disabled={loading}
+          onClick={handleSubmit}
+          className='!mb-5'
+          fullWidth
+          variant='contained'
+        >
+          Đăng nhập
+        </Button>
+        <p style={{ textAlign: 'center' }}>
+          <span>Chưa có tài khoản? </span>
+          <Link
+            disabled={loading}
+            style={{
+              [loading ? 'pointerEvents' : '']: 'none',
+              textDecoration: 'underline',
+              textAlign: 'center',
+            }}
+            to='/register'
+          >
+            Đăng ký
+          </Link>
+        </p>
+        <p style={{ textAlign: 'center' }}>
+          <Link
+            disabled={loading}
+            style={{
+              [loading ? 'pointerEvents' : '']: 'none',
+              textDecoration: 'underline',
+              textAlign: 'center',
+            }}
+            to='/forget-password'
+          >
+            Quên mật khẩu
+          </Link>
         </p>
       </Box>
     </div>
