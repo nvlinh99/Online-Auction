82ae124c1dfb4d66b34f8e28b2ff477dad0367ef
Louis Phan <louis.phhh@gmail.com>
2021-10-10 02:49:10 +0700

    update upload api

--- api/src/route/upload/index.js
+++ api/src/route/upload/index.js
@@ -2,12 +2,44 @@ const _ = require('lodash')
 const { Router, } = require('express')
 const { promisify, } = require('util')
 const shortid = require('shortid')
+const uploadService = require('../../service/upload-service')
 
 exports.path = '/upload'
 
 const productRouter = Router()
+// productRouter.post('/img',  require('../../middleware/auth').authorize, async (req, res) => {
+// // productRouter.post('/img', async (req, res) => {
+//   const imgList = _.get(req, 'files', null)
+//   if (_.isEmpty(imgList)) {
+//     return res.json({
+//       code: 1000,
+//       data:{ loaded: [], },
+//     }) 
+//   }
+
+//   // __staticPath
+//   const tasks = []
+//   const uploaded = []
+//   for (let i = 0; ;i += 1) {
+//     const img = imgList[i]
+//     if (!img) break
+//     const imgFileName = `${shortid.generate()}.${_.last(_.split(img.name, '.'))}`
+//     const imgPath = `/static/images/${imgFileName}`
+//     uploaded.push(imgPath)
+//     // eslint-disable-next-line no-undef
+//     const imgFilePath = `${__staticPath}/images/${imgFileName}`
+//     tasks.push(promisify(img.mv)(imgFilePath))
+//   }
+//   await Promise.all(tasks)
+
+//   return res.json({
+//     code: 1000,
+//     data: { uploaded, },
+//   })
+// })
+
 productRouter.post('/img',  require('../../middleware/auth').authorize, async (req, res) => {
-// productRouter.post('/img', async (req, res) => {
+  // productRouter.post('/img', async (req, res) => {
   const imgList = _.get(req, 'files', null)
   if (_.isEmpty(imgList)) {
     return res.json({
@@ -15,26 +47,40 @@ productRouter.post('/img',  require('../../middleware/auth').authorize, async (r
       data:{ loaded: [], },
     }) 
   }
-
+  
   // __staticPath
   const tasks = []
-  const uploaded = []
   for (let i = 0; ;i += 1) {
     const img = imgList[i]
     if (!img) break
-    const imgFileName = `${shortid.generate()}.${_.last(_.split(img.name, '.'))}`
-    const imgPath = `/static/images/${imgFileName}`
-    uploaded.push(imgPath)
-    // eslint-disable-next-line no-undef
-    const imgFilePath = `${__staticPath}/images/${imgFileName}`
-    tasks.push(promisify(img.mv)(imgFilePath))
+    tasks.push(uploadService.upload(imgList[0]))
   }
-  await Promise.all(tasks)
-
+  const uploadResponse = await Promise.all(tasks)
+  const uploaded = _.map(uploadResponse, 'url')
+  
   return res.json({
     code: 1000,
     data: { uploaded, },
   })
 })
+
+// productRouter.post('/test',  async (req, res) => {
+//   // productRouter.post('/img', async (req, res) => {
+//   const imgList = _.get(req, 'files', null)
+//   if (_.isEmpty(imgList)) {
+//     return res.json({
+//       code: 1000,
+//       data:{ loaded: [], },
+//     }) 
+//   }
+  
+//   const uploaded = []
+//   const result = await uploadService.upload(imgList[0])
+//   console.log(result)
+//   return res.json({
+//     code: 1000,
+//     data: { uploaded, },
+//   })
+// })
 
 exports.router = productRouter
---
+++ api/src/service/upload-service.js
@@ -0,0 +1,27 @@
+const cloudinary = require('cloudinary').v2
+const _ = require('lodash')
+const { promisify, } = require('util')
+const shortid = require('shortid')
+const fs = require('fs')
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+exports.upload = async (file) => {
+  const imgFileName = `${shortid.generate()}.${_.last(_.split(file.name, '.'))}`
+  // eslint-disable-next-line no-undef
+  const imgFilePath = `${__staticPath}/images/${imgFileName}`
+  await promisify(file.mv)(imgFilePath)
+  const result = await new Promise((res, rej) => {
+    const stream = cloudinary.uploader.upload_stream(function (err, rst) {
+      if (err) return rej(err)
+      return res((rst))
+    })
+    fs.createReadStream(imgFilePath).pipe(stream)
+  })
+  fs.unlink(imgFilePath, () => {})
+  return result
+}
