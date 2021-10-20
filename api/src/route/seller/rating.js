b099b0877eb80ab9236ff8f02248337bbcccb5cf
Vu-Linh Nguyen <nguyenvulinh99@gmail.com>
2021-10-20 23:26:40 +0700

    ThumbsUp

---
+++ api/src/route/seller/rating.js
@@ -0,0 +1,44 @@
+const joi = require('joi')
+const genRequestValidation = require('../../middleware/gen-request-validation')
+const ProductModel = require('../../model/product')
+const RatingModel = require('../../model/rating')
+const UserModel = require('../../model/user')
+
+const requestValidationHandler = genRequestValidation({
+  body: joi
+    .object({
+      comment: joi.string().trim().required().invalid('', null),
+    }),
+})
+
+exports.ratingUp = async (req, res) => {
+  const { user, } = req
+  const { bidderId, } = req.params
+  const { comment, } = req.body
+  const isBuy = await ProductModel.findOne({ sellerId: user.id, bidderId, status: 1, })
+  if (!isBuy) {
+    return res.status(400).json({
+      code: -1000,
+      message: 'Bạn không thể đánh giá!',
+    })
+  }
+
+  const vote = await RatingModel.create({
+    userId: bidderId,
+    rateById: user.id,
+    comment,
+  })
+
+  if (!vote) {
+    return res.status(400).json({
+      code: -1000,
+      message: 'Có lỗi xảy ra!',
+    })
+  }
+  const thumbsUp = 1
+  await UserModel.findOneAndUpdate({ id: bidderId, }, { $inc: { rateIncrease: thumbsUp, }, })
+}
+
+module.exports = [
+  requestValidationHandler,
+]
