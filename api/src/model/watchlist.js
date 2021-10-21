8367731d0bd572e3cc53b77712224aaac1ca6145
Thanh Hai <hthai2201@gmail.com>
2021-10-21 20:20:53 +0700

    get watch list

--- api/src/model/watchlist.js
+++ api/src/model/watchlist.js
@@ -1,12 +1,24 @@
-const mongoose = require('mongoose')
-const nanoid = require('../util/nanoid')
+const mongoose = require("mongoose")
+const mongoosePaginate = require("mongoose-paginate-v2")
+const nanoid = require("../util/nanoid")
 
-const watchListSchema = new mongoose.Schema({
-  id: { type: Number, required: true,  default: nanoid.getGenFunction(), },
-  userId: { type: Number, required: true, },
-  productId: { type: Number, required: true, },
-}, {
-  timestamps: true,
+const watchListSchema = new mongoose.Schema(
+  {
+    id: { type: Number, required: true, default: nanoid.getGenFunction() },
+    userId: { type: Number, required: true },
+    productId: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+watchListSchema.virtual("product", {
+  ref: "Product",
+  localField: "productId",
+  foreignField: "id",
 })
-
-module.exports = mongoose.model('WatchList', watchListSchema)
+watchListSchema.plugin(mongoosePaginate)
+module.exports = mongoose.model("WatchList", watchListSchema)
