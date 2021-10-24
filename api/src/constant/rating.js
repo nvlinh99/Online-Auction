3009bf8f9dcc04478a8328d67b0c031776c0524c
Louis Phan <louis.phhh@gmail.com>
2021-10-24 22:14:41 +0700

    add rating type

---
+++ api/src/constant/rating.js
@@ -0,0 +1,4 @@
+exports.RATING_TYPE = {
+  LIKE: 0,
+  DISLIKE: 1
+}
--- api/src/model/rating.js
+++ api/src/model/rating.js
@@ -5,6 +5,7 @@ const ratingSchema = new mongoose.Schema({
   id: { type: Number, required: true,default: nanoid.getGenFunction(), },
   userId: { type: Number, required: true, },
   rateById: { type: Number, required: true, },
+  type: { type: Number, required: true },
   comment: { type: String, },
 }, {
   timestamps: true,
