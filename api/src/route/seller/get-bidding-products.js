19bc8f15b722f89a6a10e3253a26e7f58e479269
Thanh Hai <hthai2201@gmail.com>
2021-10-27 19:03:19 +0700

    seller api

---
+++ api/src/route/seller/get-bidding-products.js
@@ -0,0 +1,65 @@
+const joi = require("joi")
+const ProductService = require("../../service/product-service")
+const genRequestValidation = require("../../middleware/gen-request-validation")
+const UserModel = require("../../model/user")
+const ProductModel = require("../../model/product")
+const WatchlistModel = require("../../model/watchlist")
+const BidModel = require("../../model/bid")
+
+const requestValidationHandler = genRequestValidation({
+  body: joi
+    .object({
+      page: joi.number().integer().positive().invalid(null),
+      limit: joi.number().integer().positive().invalid(null),
+      filterType: joi.string().trim(),
+    })
+    .unknown(false),
+})
+
+const handler = async (req, res) => {
+  const { page = 1, limit = 25, filterType } = req.body
+  const { id: userId } = req.user
+  const queryObj = {
+    sellerId: userId,
+    expiredDate: {
+      $gte: new Date(),
+    },
+    winnerId: null,
+  }
+
+  const data = await ProductModel.paginate(queryObj, {
+    page,
+    limit,
+    populate: [
+      "totalBids",
+      {
+        path: "currentBid",
+        populate: "bidder",
+        match: { status: 0 },
+        options: {
+          sort: {
+            price: -1,
+          },
+        },
+      },
+    ],
+    sort: {
+      expiredDate: -1,
+      currentPrice: -1,
+    },
+  })
+  if (!data) {
+    return res.reqF({
+      message: "Lấy danh sách  sản phẩm thất bại",
+    })
+  }
+
+  return res.reqS({
+    totalItems: data.totalDocs,
+    items: data.docs,
+    totalPages: data.totalPages,
+    currentPage: data.page,
+  })
+}
+
+module.exports = [requestValidationHandler, handler]
---
+++ api/src/route/seller/get-won-products.js
@@ -0,0 +1,59 @@
+const joi = require("joi")
+const ProductService = require("../../service/product-service")
+const genRequestValidation = require("../../middleware/gen-request-validation")
+const UserModel = require("../../model/user")
+const ProductModel = require("../../model/product")
+const WatchlistModel = require("../../model/watchlist")
+
+const requestValidationHandler = genRequestValidation({
+  body: joi
+    .object({
+      page: joi.number().integer().positive().invalid(null),
+      limit: joi.number().integer().positive().invalid(null),
+      filterType: joi.string().trim(),
+    })
+    .unknown(false),
+})
+
+const handler = async (req, res) => {
+  const { page = 1, limit = 25, filterType } = req.body
+  const { id: userId } = req.user
+  const queryObj = {
+    sellerId: userId,
+    winnerId: {
+      $ne: null,
+    },
+  }
+
+  const data = await ProductModel.paginate(queryObj, {
+    page,
+    limit,
+    populate: [
+      "totalBids",
+      {
+        path: "currentBid",
+        populate: "bidder",
+        match: { status: 0 },
+        options: {
+          sort: {
+            price: -1,
+          },
+        },
+      },
+    ],
+  })
+  if (!data) {
+    return res.reqF({
+      message: "Lấy danh sách  sản phẩm thất bại",
+    })
+  }
+
+  return res.reqS({
+    totalItems: data.totalDocs,
+    items: data.docs,
+    totalPages: data.totalPages,
+    currentPage: data.page,
+  })
+}
+
+module.exports = [requestValidationHandler, handler]
